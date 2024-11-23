8f50a87dbb7b5d95aa3e7730acea9b898f49859a
loqum <ruben693@gmail.com>
2024-11-23 11:25:57 +0100

    Cambia botón de scroll a MainLayout.
    Interesa tenerlo en toda la web.

--- web-service/src/layouts/MainLayout.jsx
+++ web-service/src/layouts/MainLayout.jsx
@@ -1,10 +1,35 @@
 import NavigationBar from "../components/NavigationBar.jsx";
+import {ArrowUp} from "react-bootstrap-icons";
 
 function MainLayout({children}) {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
     return (
         <>
             <NavigationBar/>
             <main>{children}</main>
+            <div
+                onClick={scrollToTop}
+                style={{
+                    position: "fixed",
+                    bottom: "20px",
+                    right: "20px",
+                    backgroundColor: "#007bff",
+                    color: "white",
+                    borderRadius: "50%",
+                    padding: "10px",
+                    cursor: "pointer",
+                    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+                    zIndex: 1000,
+                }}
+                aria-label="Volver al inicio"
+            >
+                <ArrowUp size={24}/>
+            </div>
         </>
     );
 
--- web-service/src/pages/HomePage.jsx
+++ web-service/src/pages/HomePage.jsx
@@ -1,16 +1,8 @@
 import {Container, Image} from "react-bootstrap";
 import logo from "../assets/images/logo.png";
 import MoviesCards from "../components/MoviesCards.jsx";
-import {ArrowUp} from "react-bootstrap-icons";
 
 function HomePage() {
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
-    };
 
     return (
         <>
@@ -18,24 +10,6 @@ function HomePage() {
                 <Image src={logo} alt="Logo MUBISearch" fluid rounded className="mb-4 mt-4"/>
                 <h1>Últimas películas</h1>
                 <MoviesCards/>
-                <div
-                    onClick={scrollToTop}
-                    style={{
-                        position: "fixed",
-                        bottom: "20px",
-                        right: "20px",
-                        backgroundColor: "#007bff",
-                        color: "white",
-                        borderRadius: "50%",
-                        padding: "10px",
-                        cursor: "pointer",
-                        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-                        zIndex: 1000,
-                    }}
-                    aria-label="Volver al inicio"
-                >
-                    <ArrowUp size={24}/>
-                </div>
             </Container>
         </>
     )
